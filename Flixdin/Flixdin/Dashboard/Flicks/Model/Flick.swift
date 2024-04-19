240db6195ffa1c26d1ed5cd83cfa572acac76c62
Prashanna <prashanna.rajbhandari1@gmail.com>
2024-04-20 00:10:01 +0530

    feat: added models for flix

--- Flixdin/Flixdin/Dashboard/Flicks/Model/Flick.swift
+++ Flixdin/Flixdin/Dashboard/Flicks/Model/Flick.swift
@@ -5,9 +5,9 @@
 //  Created by Sanjeev RM on 26/08/23.
 //
 
+import AVKit
 import Foundation
 import SwiftUI
-import AVKit
 
 struct Flick: Identifiable {
     var id = UUID().uuidString
@@ -15,8 +15,48 @@ struct Flick: Identifiable {
     var mediaFile: MediaFile
 }
 
-
-struct FlickRequest: Encodable{
+struct FlickRequest: Encodable {
     let id: String
     let file: String
+}
+
+// MARK: NewFlixRequest
+
+struct NewFlixRequest: Encodable {
+    let ownerid: Int
+    let domain: String
+    let caption: String
+    let applicants: Int
+    let location: String
+    let likes: [String]
+    let flixurl: String
+    let flixdate: String
+    let comments: [String]
+}
+
+// MARK: DeleteFlixRequest
+
+struct DeleteFlixRequest: Encodable {
+    let flixid: Int
+}
+
+// MARK: UpdateFlixRequest
+
+struct UpdateFlixRequest: Encodable {
+    let flixid: Int
+    let fieldname: String
+    let value: String
+}
+
+// MARK: LikeOrDislikeFlixRequest
+
+struct LikeOrDislikeFlixRequest: Encodable {
+    let flixid: Int
+    let userid: Int
+}
+
+// MARK: APIResponse
+
+struct APIResponse: Decodable {
+    let message: String
 }
