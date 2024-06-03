53f22e0f1345603a4280730e5f3790363d5286e7
Prashanna <prashanna.rajbhandari1@gmail.com>
2024-06-03 23:11:49 +0545

    feat: show searched chat list

--- Flixdin/Flixdin/Messages/View/AllMessagesView.swift
+++ Flixdin/Flixdin/Messages/View/AllMessagesView.swift
@@ -8,49 +8,51 @@
 import SwiftUI
 
 struct AllMessagesView: View {
-    
     @EnvironmentObject var homeViewModel: HomeViewModel
-    
+
     @State var isTyping: Bool = false
     @State var lastMessage: String = ""
     @State var unreadMessagesCount: Int = 1
     @State var userSearchQuery: String = ""
-    
+
     @StateObject var socketManager = SocketIOManager()
     @StateObject var chatViewModel = ChatViewModel()
-    
-    
+
     var body: some View {
-        
         NavigationView {
-            VStack (alignment: .leading) {
+            VStack(alignment: .leading) {
                 ScrollView {
                     VStack(spacing: 16) {
-                        
 //                        onlineUsersView
 
 //                        messageTypeView
-                        
+
                         Divider()
-                        
-                        messagesListRow()
+
+//                        messagesListRow()
+
+                        if userSearchQuery.isEmpty {
+                            messagesListRow()
+                        } else {
+                            searchResultsView() 
+                        }
                     }
                 }
             }
-            
+
             .searchable(text: $userSearchQuery, placement: .navigationBarDrawer(displayMode: .automatic))
             .navigationBarItems(leading: backButtonAndTitle)
             .onSubmit(of: .search) {
-                Task{
+                Task {
                     await chatViewModel.getSearchedUser()
                 }
             }
             .onAppear(perform: {
                 socketManager.connect()
-                Task{
+                Task {
                     await chatViewModel.getChats()
                 }
-                
+
             })
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(flixColor: .backgroundPrimary))
@@ -59,14 +61,11 @@ struct AllMessagesView: View {
 }
 
 extension AllMessagesView {
-    
-    //MARK: - Back Button and Title
-    
+    // MARK: - Back Button and Title
+
     private var backButtonAndTitle: some View {
-        
         HStack(spacing: 16) {
-            
-            Button{
+            Button {
                 // Dismiss the message view and go back to the home view
                 socketManager.disconnect()
                 homeViewModel.showMessages = false
@@ -74,15 +73,30 @@ extension AllMessagesView {
                 Image(systemName: "chevron.left")
                     .foregroundColor(Color(flixColor: .lightOlive))
             }
-            
+
             Text("Messages")
                 .font(.system(.title, weight: .bold))
                 .foregroundColor(Color(flixColor: .lightOlive))
         }
     }
-    
-    //MARK: - SearchBar
-    
+
+    @ViewBuilder
+    private func searchResultsView() -> some View {
+        if chatViewModel.searchedUser.isEmpty {
+            Text("No users found.")
+                .foregroundColor(.gray)
+        } else {
+            VStack(alignment: .leading) {
+                ForEach(chatViewModel.searchedUser) { user in
+
+                    searchedUserRow(user: user)
+                }
+            }
+        }
+    }
+
+    // MARK: - SearchBar
+
     private var searchBarView: some View {
         SearchFieldView(backgroundColor: Color(flixColor: .backgroundSecondary), searchQuery: $userSearchQuery)
             .padding([.horizontal, .bottom])
@@ -90,31 +104,60 @@ extension AllMessagesView {
             .opacity(0.8)
 //        TextField("", text: $userSearchQuery)
     }
-    
-    //MARK: - MessagesListView
+
+    // MARK: Searched User Row
+
+    private func searchedUserRow(user: SearchedUserResponse) -> some View {
+        HStack {
+            AsyncImage(url: URL(string: user.profilepic ?? "")) { image in
+                image.resizable()
+            } placeholder: {
+                Image(systemName: "person.fill")
+                    .resizable()
+            }
+            .frame(width: 50, height: 50)
+            .clipShape(Circle())
+
+            VStack(alignment: .leading) {
+                Text(user.fullname)
+                    .foregroundColor(.black)
+
+                    .fontWeight(.medium)
+                Text("@\(user.username)")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+
+            Spacer()
+        }
+        .padding()
+    }
+
+    // MARK: - MessagesListView
+
     // Get all the users and display them here in the vstack
     private func messagesListRow() -> some View {
-        VStack{
+        VStack {
             HStack {
                 NavigationLink {
                     DirectMessagesView()
                         .navigationBarBackButtonHidden()
-                } label : {
+                } label: {
                     HStack(spacing: 16) {
                         Circle()
                             .stroke(lineWidth: 4)
                             .foregroundColor(.init(flixColor: .lightOlive))
                             .frame(height: 50)
-                            .overlay (
+                            .overlay(
                                 GeometryReader { proxy in
                                     AsyncImage(url: URL(string: "https://picsum.photos/50/50")!)
                                         .frame(width: proxy.size.width, height: proxy.size.height)
                                         .aspectRatio(contentMode: .fit)
                                         .clipShape(Circle())
-                            }
-                          )
-                        
-                        VStack (alignment: .leading){
+                                }
+                            )
+
+                        VStack(alignment: .leading) {
                             Text("John Doe")
                                 .foregroundColor(.primary)
                                 .font(.system(size: 16, weight: .semibold))
@@ -124,17 +167,17 @@ extension AllMessagesView {
                                     .font(.system(size: 12))
                                     .bold()
                             } else {
-                                Text(isTyping ? "Typing...": "\(lastMessage)")
+                                Text(isTyping ? "Typing..." : "\(lastMessage)")
                                     .font(.system(size: 12, weight: .light))
                             }
                         }
                     }
-                    
+
                     Spacer()
                 }
-                
+
 //                Spacer()
-                
+
                 if unreadMessagesCount != 0 {
                     Image(systemName: "circle.fill")
                         .foregroundColor(Color(flixColor: .backgroundTernary))
@@ -152,16 +195,15 @@ extension AllMessagesView {
             .padding(.vertical, 8)
         }
     }
-    
-    //MARK: - Online Users
-    
-    private var onlineUsersView : some View {
-        ScrollView (.horizontal, showsIndicators: false) {
-            HStack (spacing: 16){
-                
+
+    // MARK: - Online Users
+
+    private var onlineUsersView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
                 // Get the data from the database and count the persons that are online and show them here.
-                ForEach(1...10, id: \.self) { i in
-                    ZStack (alignment: .topTrailing){
+                ForEach(1 ... 10, id: \.self) { _ in
+                    ZStack(alignment: .topTrailing) {
                         Circle()
                             .frame(width: 50, height: 50)
                             .foregroundColor(.clear)
@@ -170,23 +212,23 @@ extension AllMessagesView {
                                     .stroke(lineWidth: 2)
                                     .frame(width: 55, height: 55)
                                     .foregroundColor(Color(flixColor: .lightOlive))
-                        )
-                        
+                            )
+
                         AsyncImage(url: URL(string: "https://picsum.photos/50/50")!)
                             .scaledToFit()
                             .cornerRadius(50)
                             .aspectRatio(contentMode: .fill)
-                        
+
                         ZStack {
                             Circle()
                                 .foregroundColor(.white)
                                 .frame(width: 15, height: 15)
-                            
+
                             Circle()
                                 .foregroundColor(.green)
                                 .frame(width: 10, height: 10)
                         }
-                        .offset(x:6, y:0)
+                        .offset(x: 6, y: 0)
                     }
                 }
             }
@@ -195,13 +237,13 @@ extension AllMessagesView {
             .padding(.horizontal, 16)
         }
     }
-    
-    //MARK: - Message Type View.
-    
+
+    // MARK: - Message Type View.
+
     private var messageTypeView: some View {
         HStack {
             Spacer()
-            
+
             Button {
                 // Primary Tab
             } label: {
@@ -215,9 +257,9 @@ extension AllMessagesView {
                             .foregroundColor(Color(flixColor: .backgroundSecondary))
                     )
             }
-            
+
             Spacer()
-            
+
             Button {
                 // General Tab
             } label: {
@@ -231,7 +273,7 @@ extension AllMessagesView {
                             .foregroundColor(Color(flixColor: .backgroundSecondary))
                     )
             }
-            
+
             Spacer()
 
             Button {
@@ -247,13 +289,12 @@ extension AllMessagesView {
                             .foregroundColor(Color(flixColor: .backgroundSecondary))
                     )
             }
-            
+
             Spacer()
         }
     }
-    
-
 }
+
 struct AllMessagesView_Previews: PreviewProvider {
     static var previews: some View {
         AllMessagesView()
