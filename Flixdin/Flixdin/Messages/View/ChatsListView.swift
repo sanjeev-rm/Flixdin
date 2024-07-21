02885a72290b32d21a12dbcf25db0671f6719397
sanjeev-rm <sanjeevraghu2050@gmail.com>
2024-07-21 21:50:56 +0530

    bugfix: fixed chats list issue

---
+++ Flixdin/Flixdin/Messages/View/ChatsListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatsListView.swift
+//  Flixdin
+//
+//  Created by Sanjeev RM on 21/07/24.
+//
+
+import SwiftUI
+
+struct ChatsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatsListView()
+}
